3e397223f3240fbcca279e4e74c64e6ddfdfc32c
Timothy <reese18@purdue.edu>
2025-04-05 20:33:26 -0400

    fix

--- _plugins/fix-sphinx-paths.rb
+++ _plugins/fix-sphinx-paths.rb
@@ -1,15 +1,18 @@
 Jekyll::Hooks.register :site, :post_write do |site|
-    vh_dir = File.join(site.config['destination'], 'VirtualHumans')
-    if Dir.exist?(vh_dir)
-      # Fix CSS and JS paths in all HTML files
-      Dir.glob(File.join(vh_dir, '**', '*.html')).each do |file|
+  # List of Sphinx output folders to fix
+  %w(VirtualHumans GenerativeAI_ComputationalPsychology).each do |folder|
+    dir = File.join(site.config['destination'], folder)
+    if Dir.exist?(dir)
+      # Fix CSS and JS paths in all HTML files within the folder
+      Dir.glob(File.join(dir, '**', '*.html')).each do |file|
         content = File.read(file)
         
-        # Replace relative paths with absolute paths
-        content.gsub!('href="_static/', 'href="/VirtualHumans/_static/')
-        content.gsub!('src="_static/', 'src="/VirtualHumans/_static/')
+        # Replace relative paths with absolute paths for _static assets
+        content.gsub!('href="_static/', "href=\"/#{folder}/_static/")
+        content.gsub!('src="_static/', "src=\"/#{folder}/_static/")
         
         File.write(file, content)
       end
     end
   end
+end
