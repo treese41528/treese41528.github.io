47f59f38c8b0cdb7bccf75b6caa4da2ba9bcbc92
Timothy <reese18@purdue.edu>
2025-07-29 16:14:53 -0400

    website update

--- _plugins/copy_sphinx_docs.rb
+++ _plugins/copy_sphinx_docs.rb
@@ -6,4 +6,8 @@ Jekyll::Hooks.register :site, :post_write do |site|
   if Dir.exist?('GenerativeAI_ComputationalPsychology')
     FileUtils.cp_r('GenerativeAI_ComputationalPsychology', site.config['destination'])
   end
+
+  if Dir.exist?('STAT350')
+    FileUtils.cp_r('STAT350', site.config['destination'])
+  end
 end
